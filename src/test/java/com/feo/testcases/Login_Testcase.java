1600968e55004953a30a8f3a767f35fa6bfaa2e4
seleniumtrainer2012 <seleniumtrainer2012@gmail.com>
2018-01-22 09:10:25 +0530

    sanity pack demo project

---
+++ src/test/java/com/feo/testcases/Login_Testcase.java
@@ -0,0 +1,12 @@
+package com.feo.testcases;
+
+import org.testng.annotations.Test;
+
+public class Login_Testcase {
+	@Test
+	public void verify_create_account_valid_inputs(){
+		System.out.println("running testcase verify_create_account_valid_inputs");
+	}
+	
+
+}
